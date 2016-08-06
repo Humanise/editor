aa23ec6aaa3aa450aaf065686f50d45da7c85609
Jonas Munk <jonasmunk@mac.com>
2016-08-06 12:45:57 +0200

    More robust stream editing

--- Editor/Tools/Builder/streams.js
+++ Editor/Tools/Builder/streams.js
@@ -3,7 +3,7 @@ hui.ui.listen({
 
   $open$selector : function(item) {
     if (item.kind=='stream') {
-      this.editStream(item.value);
+      streamEditor.edit(item.value);
     }
   },
   $select$selector : function(item) {
@@ -20,58 +20,11 @@ hui.ui.listen({
     list.refresh();
   },
 
-  editStream : function(id) {
-    sourceFormula.reset();
-    hui.ui.request({
-      message : {start:{en:'Loading source...',da:'Henter kilde...'},delay:300},
-      parameters : {id:id},
-      url : '../../Services/Model/LoadObject.php',
-      $object : function(stream) {
-        this.streamId = stream.id;
-        streamFormula.setValues(stream);
-        deleteStream.setEnabled(true);
-        streamWindow.show();
-      }.bind(this)
-    });
-  },
-
-  // Source
-  $click$cancelStream : function() {
-    streamFormula.reset();
-    streamWindow.hide();
-  },
-  $click$saveStream : function() {
-    var data = streamFormula.getValues();
-    data.id = this.streamId;
-    hui.ui.request({
-      url : 'actions/SaveStream.php',
-      json : {data:data},
-      $success : function() {
-        this.streamId = null;
-        streamFormula.reset();
-        streamWindow.hide();
-        selector.refresh();
-      }.bind(this)
-    });
-  },
-  $click$deleteStream : function() {
-    hui.ui.request({
-      url : '../../Services/Model/DeleteObject.php',
-      parameters : {id : this.streamId},
-      $success : function() {
-        this.streamId = null;
-        streamFormula.reset();
-        streamWindow.hide();
-        selector.refresh();
-      }.bind(this)
-    });
+  $changed$streamEditor : function() {
+    selectionSource.refresh();
   },
 
   $click$newStream : function() {
-    this.streamId = null;
-    streamFormula.reset();
-    streamWindow.show();
-    deleteStream.setEnabled(false);
-    streamFormula.focus();
+    streamEditor.makeNew()
   }
 });
