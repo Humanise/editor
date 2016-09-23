7d7b85d9b62191039782dbebeb47aa3444638c7d
Jonas Munk <jonasmunk@mac.com>
2016-09-23 21:37:22 +0200

    Better indentation

--- Editor/Classes/Modules/Review/ReviewService.php
+++ Editor/Classes/Modules/Review/ReviewService.php
@@ -9,12 +9,12 @@ if (!isset($GLOBALS['basePath'])) {
 	exit;
 }
 class ReviewService {
-	
+
 	static function search($query) {
 		$sql = '';
 		if ($query['unreviewed']) {
-			$sql = "select page.id as page_id, page.title as page_title,'' as user_title, null as date, -1 as accepted 
-					from page where page.id not in (select relation.from_object_id from relation,review 
+			$sql = "select page.id as page_id, page.title as page_title,'' as user_title, null as date, -1 as accepted
+					from page where page.id not in (select relation.from_object_id from relation,review
 					where relation.to_type='object' and relation.to_object_id=review.object_id)
 					order by date desc,page_title";
 		}
@@ -23,7 +23,7 @@ class ReviewService {
 				$sql.=' union ';
 			}
 			$sql.= "select page.id as page_id,page.title as page_title,user.title as user_title,UNIX_TIMESTAMP(review.date) as date,review.accepted
-					from page,relation as page_review,relation as review_user,review,object as user 
+					from page,relation as page_review,relation as review_user,review,object as user
 					where page_review.from_type='page' and page_review.from_object_id=page.id
 					and page_review.to_type='object' and page_review.to_object_id=review.object_id
 					and review_user.from_type='object' and review_user.from_object_id=review.object_id
@@ -39,8 +39,8 @@ class ReviewService {
 			} else if ($span=='week') {
 				$sql.=' and review.date<'.Database::datetime(Dates::addDays(time(),-7));
 			}
-			
-		}	
+
+		}
 		$list = array();
 		$result = Database::select($sql);
 		while ($row = Database::next($result)) {
