e672344470e34e6c8da792b5bcad730064ddc3c6
Jonas Munk <jonasmunk@mac.com>
2016-11-21 21:52:26 +0100

    Better formatting

--- Editor/Classes/Services/ZipService.php
+++ Editor/Classes/Services/ZipService.php
@@ -10,12 +10,12 @@ if (!isset($GLOBALS['basePath'])) {
 require_once $basePath.'Editor/Libraries/pclzip/pclzip.lib.php';
 
 class ZipService {
-	
+
 	static function uploadIsZipFile($name='file') {
 		$mimes = array("application/x-zip-compressed","application/zip");
 		return in_array($_FILES[$name]["type"],$mimes);
 	}
-	
+
 	static function getArchive($path) {
 		$zip = new PclZip($path);
 		return new ZipFile($zip);
