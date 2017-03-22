69770161f2eb382e42c4e7ef1e04468cda8f56a8
Jonas Munk <jonas@humanise.dk>
2017-03-22 10:31:46 +0100

    Re-use existing variable

--- style/jonasmunk/js/vitae.js
+++ style/jonasmunk/js/vitae.js
@@ -3,7 +3,7 @@ hui.onReady(function() {
     var scrl = document.body.scrollTop || document.documentElement.scrollTop
     hero.style.filter = 'blur(' + Math.min(20,scrl/20) + 'px) brightness(' + Math.max(.5,1 - scrl/400) + ')'
     //   saturate(' + Math.min(1,document.body.scrollTop/-150 + 1) + ') 
-    var scale = Math.min(0,document.body.scrollTop) / -100 + 1;
+    var scale = Math.min(0,scrl) / -100 + 1;
     scale = Math.log(scale)*.6 + 1
     hero.style.transform = 'scale(' + scale + ') translate3d(0, -' + Math.max(0,scrl/5) + 'px, 0)'
   }
