5fcff60474473cf0eedd53ca79a35d1e5d3015be
Jonas Munk <jonas@humanise.dk>
2017-04-13 17:34:47 +0200

    Better formatting

--- Editor/Classes/Utilities/Console.php
+++ Editor/Classes/Utilities/Console.php
@@ -5,24 +5,24 @@
  */
 
 if (!isset($GLOBALS['basePath'])) {
-	header('HTTP/1.1 403 Forbidden');
-	exit;
+  header('HTTP/1.1 403 Forbidden');
+  exit;
 }
 class Console {
-    
-    static function isFromConsole() {
-        return php_sapi_name()==='cli';
-    }
-    
-    static function exitIfNotConsole() {
-        if (!Console::isFromConsole()) {
-            Response::forbidden('You are being watched!');
-            exit;
-        }
-    }
-    
-    static function getArguments() {
-        global $argv;
-        return $argv;
-    }
+
+  static function isFromConsole() {
+    return php_sapi_name()==='cli';
+  }
+
+  static function exitIfNotConsole() {
+    if (!Console::isFromConsole()) {
+      Response::forbidden('You are being watched!');
+        exit;
+      }
+  }
+
+  static function getArguments() {
+    global $argv;
+    return $argv;
+  }
 }
