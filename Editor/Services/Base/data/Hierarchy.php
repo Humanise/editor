a52fae0ccaeea3ede43b0ce02204d0ade3e7e9d0
Jonas Munk <jonas@humanise.dk>
2017-07-30 18:26:20 +0200

    Better code formatting

--- Editor/Services/Base/data/Hierarchy.php
+++ Editor/Services/Base/data/Hierarchy.php
@@ -23,35 +23,35 @@ foreach ($hierarchies as $hierarchy) {
   if ($hierarchy->getChanged() > $hierarchy->getPublished()) {
     $title .= ' !';
   }
-    $writer->startItem([
-        'icon' => 'common/hierarchy',
-        'value' => $hierarchy->getId(),
-        'title' => $title,
-        'kind' => 'hierarchy'
-    ]);
+  $writer->startItem([
+    'icon' => 'common/hierarchy',
+    'value' => $hierarchy->getId(),
+    'title' => $title,
+    'kind' => 'hierarchy'
+  ]);
   encodeLevel(0,$hierarchy->getId(),$writer);
-    $writer->endItem();
+  $writer->endItem();
 }
 $writer->endItems();
 
 function encodeLevel($parent,$hierarchyId,$writer) {
-    $sql = "select hierarchy_item.*,page.disabled,page.path,page.id as pageid from hierarchy_item" .
-      " left join page on page.id = hierarchy_item.target_id and (hierarchy_item.target_type='page' or hierarchy_item.target_type='pageref')" .
-      " where parent=" . Database::int($parent) .
-      " and hierarchy_id=" . Database::int($hierarchyId) .
-      " order by `index`";
-    $result = Database::select($sql);
-    while ($row = Database::next($result)) {
+  $sql = "select hierarchy_item.*,page.disabled,page.path,page.id as pageid from hierarchy_item" .
+    " left join page on page.id = hierarchy_item.target_id and (hierarchy_item.target_type='page' or hierarchy_item.target_type='pageref')" .
+    " where parent=" . Database::int($parent) .
+    " and hierarchy_id=" . Database::int($hierarchyId) .
+    " order by `index`";
+  $result = Database::select($sql);
+  while ($row = Database::next($result)) {
     $icon = Hierarchy::getItemIcon($row['target_type']);
     if ($row['target_type'] == 'page' && !$row['pageid']) {
       $icon = "common/warning";
     }
-        $writer->startItem([
-            'icon' => $icon,
-            'value' => $row['target_id'],
-            'title' => $row['title'],
-            'kind' => $row['target_type']
-        ]);
+    $writer->startItem([
+      'icon' => $icon,
+      'value' => $row['target_id'],
+      'title' => $row['title'],
+      'kind' => $row['target_type']
+    ]);
     encodeLevel($row['id'],$hierarchyId,$writer);
     $writer->endItem();
   }
