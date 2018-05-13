0acecead7b70ac03af938bffe8a4f2f9a60ea39b
Jonas Munk <jonas@humanise.dk>
2018-05-13 20:33:16 +0200

    New version

--- Editor/Classes/Core/SystemInfo.php
+++ Editor/Classes/Core/SystemInfo.php
@@ -6,8 +6,8 @@ if (!isset($GLOBALS['basePath'])) {
 
 class SystemInfo {
 
-  private static $date = 27;
-  private static $month = 3;
+  private static $date = 13;
+  private static $month = 5;
   private static $year = 2018;
   private static $feedbackMail = "jonasmunk@mac.com";
   private static $feedbackName = "Jonas Munk";
