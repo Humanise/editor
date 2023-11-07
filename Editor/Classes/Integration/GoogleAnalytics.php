24c93b3c9868bf49be2b50bfcc376d12879d0678
Jonas Munk <jonas@humanise.dk>
2023-11-07 18:30:45 +0100

    Disable use of Google analytics api

--- Editor/Classes/Integration/GoogleAnalytics.php
+++ Editor/Classes/Integration/GoogleAnalytics.php
@@ -7,7 +7,7 @@ if (!isset($GLOBALS['basePath'])) {
   header('HTTP/1.1 403 Forbidden');
   exit;
 }
-require_once($basePath . "Editor/Libraries/gapi/gapi.class.php");
+//require_once($basePath . "Editor/Libraries/gapi/gapi.class.php");
 
 class GoogleAnalytics {
 
